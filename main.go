f9723c287a89f5a6796a83f8a68fe5b2e025e777
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-12-05 18:39:48 +0200

    Rought sketch of the Rest API.
    Also will use Pusher (as a messaging queue)

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"os"
 
@@ -11,13 +10,32 @@ import (
 
 // Post that handles Gleb's upstream
 
+type DriverStatus struct {
+	Id             int
+	Email          string
+	Phone          string
+	FirstName      string
+	MiddleName     string
+	LastName       string
+	Capacity       int
+	CarClassId     int
+	Note           string
+	AcceptsRides   bool
+	OnTheRide      bool
+	CarStatusId    int
+	CoordLatitude  float64
+	CoordLongitude float64
+}
+
 // Accepts Driver by id and checks its status using the database,
 // returns the JSON-encoded result
-func DriverGetStatus(w http.ResponseWriter, req *http.Request) {
-	foo := map[string]string{"name": "foo"}
+func GetDriverStatus(w http.ResponseWriter, req *http.Request) {
+
+	// use the DB instead
+	status := DriverStatus{id: 0}
+
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Println(foo)
-	json.NewEncoder(w).Encode(&foo)
+	json.NewEncoder(w).Encode(&status)
 }
 
 /* This is done using the Pusher API
@@ -34,8 +52,8 @@ func UpdateDriversLocation(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/driver", DriverStatus).Methods("GET")
-	router.HandleFunc("/api/driver", DriverAcceptTrip).Methods("POST")
+	router.HandleFunc("/api/driver/{driver_id}", GetDriverStatus).Methods("GET")
+	// router.HandleFunc("/api/driver", DriverAcceptTrip).Methods("POST")
 	http.ListenAndServe(":"+os.Getenv("PORT"), router)
 
 }
