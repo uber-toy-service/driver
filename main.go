efd42f1af1f5f37d56c0ba4901313e96c52a5f26
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-12-05 15:30:57 +0200

    Encode JSON.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -8,17 +9,33 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Post that handles Gleb's upstream
+
+// Accepts Driver by id and checks its status using the database,
+// returns the JSON-encoded result
 func DriverGetStatus(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "GET request")
+	foo := map[string]string{"name": "foo"}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Println(foo)
+	json.NewEncoder(w).Encode(&foo)
 }
 
+/* This is done using the Pusher API
 func DriverAcceptTrip(w http.ResponseWriter, req *http.Request) {
-	//
+	// driver_id
+	// read json
+        }
+*/
+
+// Updates the location of the Driver
+func UpdateDriversLocation(w http.ResponseWriter, req *http.Request) {
+
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/driver", DriverGetStatus).Methods("GET")
+	router.HandleFunc("/api/driver", DriverStatus).Methods("GET")
 	router.HandleFunc("/api/driver", DriverAcceptTrip).Methods("POST")
 	http.ListenAndServe(":"+os.Getenv("PORT"), router)
+
 }
