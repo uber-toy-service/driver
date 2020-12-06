1d728aeac41ba15d6a202df772a9488ee2f86ff7
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-12-06 03:22:23 +0200

    Add more stubs.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,6 +15,10 @@ import (
 var (
 	ErrFile   *log.Logger
 	DebugFile *log.Logger
+)
+
+const (
+	SupplyLocaiton string = "https://toy-supply-location-service.herokuapp.com"
 )
 
 func init() {
@@ -30,7 +36,15 @@ func init() {
 
 /// Business logic
 
+// TODO Discuss trip cancellation
+
 // TODO Post that handles Gleb's upstream
+
+// type
+
+func ReceiveTripBroadcastFromUpstream(w http.ResponseWriter, req *http.Request) {
+
+}
 
 // TODO Use real data, for now, mock everything
 
@@ -67,6 +81,15 @@ func GetDriverStatus(w http.ResponseWriter, req *http.Request) {
 
 // DONE
 
+type LocationWithIdMsg struct {
+	id       string   `json:"id"`
+	location Location `json:"location"`
+}
+
+type LocationMsg struct {
+	Location Location `json:"location"`
+}
+
 type Location struct {
 	Longtitude float64 `json:"longtitude"`
 	Latitude   float64 `json:"latitude"`
@@ -76,21 +99,40 @@ type UpdateDriversLocationResponse struct {
 	Status string `json:"status"`
 }
 
+func PassDriversLocationToSupply(msg LocationWithIdMsg) []byte {
+	msgJSON, _ := json.Marshal(msg)
+	buffer := bytes.NewBuffer(msgJSON)
+	req, _ := http.NewRequest("POST", SupplyLocaiton+"/update-supply", buffer)
+	req.Header.Set("Content-Type", "application/json")
+	resp, _ := http.DefaultClient.Do(req)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	return body
+}
+
+// TODO Work with Yegor
+func StoreDriversLocationInDB(msg LocationWithIdMsg) []byte {
+	return []byte(`foo`)
+}
+
 // Updates Driver's location in the database
 func UpdateDriversLocation(w http.ResponseWriter, req *http.Request) {
 	driver_id := mux.Vars(req)["driver_id"]
 	DebugFile.Println("Driver's id=", driver_id)
-	var location Location
+	var location LocationMsg
 	err := json.NewDecoder(req.Body).Decode(&location)
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "Cannot update the location of the driver due to invalid JSON passed", http.StatusUnprocessableEntity)
 		return
 	}
-	/*
-		location.Latitude += 1
-		location.Longtitude += 1
-	*/
+
+	locationWithIdMsg := LocationWithIdMsg{id: driver_id, location: location.Location}
+	_ = PassDriversLocationToSupply(locationWithIdMsg)
+
+	// Send to DB
+
+	// Respond to Rostyk
 	status := UpdateDriversLocationResponse{Status: "ok"}
 	json.NewEncoder(w).Encode(status)
 }
@@ -107,5 +149,6 @@ func main() {
 	router.HandleFunc("/api/driver/{driver_id}", GetDriverStatus).Methods("GET").Headers("Content-Type", "application/json")
 	router.HandleFunc("/api/driver/{driver_id}", UpdateDriversLocation).
 		Methods("POST")
+	router.HandleFunc("/api/driver/trip_broadcast", ReceiveTripBroadcastFromUpstream).Methods("GET").Headers("Content-Type", "application/json")
 	http.ListenAndServe(":"+os.Getenv("PORT"), router)
 }
